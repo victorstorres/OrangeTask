ad7861162f851b26fba2417e3d8c4b78957b4473
Victor Torres <Vhugosoria2@gmail.com>
2024-07-09 17:05:33 -0300

    Correções de imports

--- app/src/main/java/com/example/orangetask/ui/home/HomeScreenUiState.kt
+++ app/src/main/java/com/example/orangetask/ui/home/HomeScreenUiState.kt
@@ -1,8 +1,6 @@
 package com.example.orangetask.ui.home
 
 import com.example.orangetask.data.Product
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
 
 data class HomeScreenUiState(
     val products: List<Product> = emptyList(),
--- app/src/main/java/com/example/orangetask/ui/login/InitialScreen.kt
+++ app/src/main/java/com/example/orangetask/ui/login/InitialScreen.kt
@@ -12,9 +12,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.LinearProgressIndicator
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
